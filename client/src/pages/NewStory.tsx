a14c2c02f40a1864c48b7bc3568101f7333a3f61
Maki44 <sebhatumakda20@gmail.com>
2022-05-13 17:50:21 +0200

    ipfs upload added

--- client/src/pages/NewStory.tsx
+++ client/src/pages/NewStory.tsx
@@ -1,9 +1,42 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./NewStory.css";
 import logo from "../images/medium.png";
 import { ConnectButton } from "web3uikit";
 import { Link } from "react-router-dom";
+import { useMoralisFile } from "react-moralis";
 const NewStory: FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [ipfsUrl, setIpfsUrl] = useState();
+  const { saveFile } = useMoralisFile();
+
+  const uploadFile = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // @ts-ignore
+    const textArray = text.split();
+    const metadata = {
+      title,
+      text: textArray,
+    };
+
+    saveFile(
+      "myblog.json",
+      { base64: btoa(JSON.stringify(metadata)) },
+      {
+        type: "base64",
+        saveIPFS: true,
+
+        onSuccess: (result) => {
+          // @ts-ignore
+          console.log("ipfs", result.ipfs());
+          // @ts-ignore
+          setIpfsUrl(result.ipfs());
+        },
+        onError: (error) => console.log(error),
+      }
+    );
+  };
+
   return (
     <>
       <div className="writeNav">
@@ -17,7 +50,7 @@ const NewStory: FC = () => {
         </div>
       </div>
       <div className="write">
-        <form className="writeForm">
+        <form onSubmit={uploadFile} className="writeForm">
           <div className="writeFormGroup">
             <label htmlFor="fileInput">
               <i className="writeIcon fas fa-plus"></i>
@@ -28,6 +61,8 @@ const NewStory: FC = () => {
               placeholder="Title"
               type="text"
               autoFocus={true}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <div className="writeFormGroup">
@@ -35,6 +70,8 @@ const NewStory: FC = () => {
               className="writeInput writeText"
               placeholder="Tell your story..."
               autoFocus={true}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
             />
           </div>
           <button className="writeSubmit" type="submit">
